72d8a0023d4d4fd5dd1910d6d35fea3af8ef8f97
JrKaiHu <JrKaiHu@users.noreply.github.com>
2017-06-18 18:20:15 +0800

    Firebase realtime database
    1. Get the contacts from firebase realtime database.
    2. Using listview to display the content of contacts.

---
+++ FireBaseDatabaseTest/app/src/main/java/com/example/arieshu/firebasedatabasetest/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.arieshu.firebasedatabasetest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
+
+    private ArrayList<Profile> arrayList = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+
+        DatabaseReference reference_contacts = FirebaseDatabase.getInstance().getReference("contacts");
+        reference_contacts.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                final long nChildCnt = dataSnapshot.getChildrenCount();
+                adapter.clear();
+                for ( DataSnapshot ds : dataSnapshot.getChildren() ) {
+
+                    //Profile profile = ds.getValue(Profile.class);
+                    //String name = ds.child("name").getValue().toString();
+                    //arrayList.add(ds.getValue(Profile.class));
+
+                    adapter.add(ds.child("name").getValue().toString());
+                    adapter.add(ds.child("addr").getValue().toString());
+                    adapter.add(ds.child("phone").getValue().toString());
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("contacts");
+        myRef.child("1").child("addr").setValue("上海");
+
+        String profileId = reference_contacts.push().getKey();
+
+        // creating Profile object
+        //Profile profile = new Profile("台中", "Ariel", "246801357");
+        Profile profile = new Profile();
+        profile.setAddr("台中");
+        profile.setName("Phoebe");
+        profile.setPhone("246801357");
+
+        // pushing profile to 'contacts' node using the userId
+        reference_contacts.child("3").setValue(profile);
+    }
+
+    private void initView() {
+
+        listView = (ListView) findViewById(R.id.list);
+//        adapter = new ArrayAdapter<String>(this,
+//                android.R.layout.simple_list_item_1,
+//                android.R.id.text1);
+        adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1);
+        listView.setAdapter(adapter);
+    }
+}
---
+++ FireBaseDatabaseTest/app/src/main/java/com/example/arieshu/firebasedatabasetest/Profile.java
@@ -0,0 +1,45 @@
+package com.example.arieshu.firebasedatabasetest;
+
+class Profile {
+
+    private String addr;
+    private String name;
+    private String phone;
+
+    // Default constructor required for calls to
+    // DataSnapshot.getValue(User.class)
+
+    public Profile() {
+        // Default constructor required for calls to DataSnapshot.getValue(Post.class)
+    }
+
+    public Profile(String addr, String name, String phone) {
+        this.addr = addr;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
